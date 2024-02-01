009ed6c4de939eebd711b50c526064d676128b38
AbhinavKrr <abhinavkumarblog@gmail.com>
2024-02-01 12:50:14 +0530

    pushing code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ShopCategory from './Pages/ShopCategory'
 import Product from './Pages/Product'
 import Cart from './Pages/Cart'
 import LoginSingup from './Pages/LoginSingup'
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSingup/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
